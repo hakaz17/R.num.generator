173e7568998ea92a30d10d8bb010ae6554d899c3
Darnell <apple@Darnell-Apples-MacBook-Air.local>
2021-03-01 18:12:20 +0200

    Initial commit

---
+++ app/src/main/java/com/example/rnumgenerator/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.rnumgenerator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView txt;
+    Button btn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        txt = findViewById(R.id.TextView);
+        btn = findViewById(R.id.button);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @SuppressLint("SetTextI18n")
+            @Override
+            public void onClick(View v) {
+                Random random = new Random();
+                int val = random.nextInt(1000-1)+1;
+                txt.setText(Integer.toString(val));
+            }
+        });
+
+
+
+    }
+}
